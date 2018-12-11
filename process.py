d6320e1c6cdeac127bda0f6ccd1256eb8af6c2a3
thatchayut <thatchayut.u@gmail.com>
2018-12-12 02:58:34 +0700

    create list of sample's index shuffle this list to make it's not depending on order fix separation method

--- process.py
+++ process.py
@@ -4,3 +4,44 @@ def chunks(l, n):
     for i in range(0, len(l), n):
         # Create an index range for l of n items:
         yield l[i:i+n]
+
+def getInput(num_of_data):  
+    while True:
+        num_of_folds = input("Number of folds: ")
+        if (num_of_folds.isnumeric() == False):
+            print("WARNING : Invalid input must be numeric.")
+        elif (int(num_of_folds) > num_of_data):
+            print("WARNING : Number of folds exceeds the size of the dataset.")
+        elif (int(num_of_folds) <= 1):
+            print("WARNING : Number of folds cannot lower than or equal to 1.")
+        else:
+            break
+    num_of_folds = int(num_of_folds)
+
+    # choose neural network architecture
+    print("#### Select your neural network architecture ####")
+    while True:
+        num_of_layers = input("Number of hidden layers : ")
+        if (num_of_layers.isnumeric() == False):
+            print("WARNING : Invalid input must be numeric")
+        elif (int(num_of_layers) < 1):
+            print("WARNING : Number of layers must greater than or equal to 1.")
+        else:
+            break
+    num_of_layers = int(num_of_layers)
+
+    # choose number or nodes in each layer
+    num_of_nodes_in_hidden_layer = []
+    # separate input data in to k parts for cross-validation
+    for i in range(0, num_of_layers):
+        while True:
+            num_of_nodes = input("Number of nodes in layer " + str(i + 1) + " : ")
+            if (num_of_nodes.isnumeric() == False):
+                print("WARNING : Invalid input must be numeric.")
+            elif (int(num_of_nodes) < 1):
+                print("WARNING : Number of nodes must greater than or equal to 1.")
+            else:
+                num_of_nodes = int(num_of_nodes)
+                num_of_nodes_in_hidden_layer.append(num_of_nodes)
+                break
+    return num_of_folds, num_of_layers, num_of_nodes_in_hidden_layer
--- pso.py
+++ pso.py
@@ -1,35 +1,38 @@
 #!/usr/bin/python
 import pandas as pd
+import numpy as np
+import random
 import math
 import process
 
 def main():
     # prepare data
-    file = pd.read_csv("AirQualityUCI.csv")
+    # default row_to_read = 9357
+    row_to_read = 100
+    file = pd.read_csv("AirQualityUCI.csv", nrows = row_to_read)
     col_to_read_input = ['PT08.S1(CO)', 'PT08.S2(NMHC)', 'PT08.S3(NOx)', 'PT08.S4(NO2)', 'PT08.S5(O3)', 'T', 'RH', 'AH']
     col_to_read_output = ['C6H6(GT)']
     file_input = pd.read_csv("AirQualityUCI.csv", usecols = col_to_read_input)
     file_output = pd.read_csv("AirQualityUCI.csv", usecols = col_to_read_output)
-    num_of_data = file_input.shape[0]
+    # num_of_data = file_input.shape[0]
+    num_of_data = row_to_read
 
-    # ask for required data
-    while True:
-        num_of_folds = input("Number of folds: ")
-        if (num_of_folds.isnumeric() == False):
-            print("WARNING : Invalid input must be numeric")
-        elif(int(num_of_folds) > num_of_data):
-            print("WARNING : Number of folds exceeds the size of a dataset")
-        elif(int(num_of_folds) <= 1):
-            print("WARNING : Number of folds cannot lower than or equal to 1")
-        else:
-            break
-    num_of_folds = int(num_of_folds)
+    # ask for required value
+    num_of_folds, num_of_hidden_layers, num_of_nodes_in_hidden_layer = process.getInput(num_of_data)
+
+    # create list of sample's index
+    list_sample_index = list(file.index)
+    # shuffle list to make it's not affected by order
+    random.shuffle(list_sample_index)
+    # print(list_sample_index)
 
     # separate input in to k chunks
     chunk_size = math.ceil(num_of_data / num_of_folds)
-    chunk_sample_input = list(process.chunks(file_input, chunk_size))
-    chunk_sample_output = list(process.chunks(file_output, chunk_size))
-    num_of_chunks = len(chunk_sample)
+    chunk_sample_index = list(process.chunks(list_sample_index, chunk_size))
+    num_of_chunks = len(chunk_sample_index)
+
+    print(chunk_sample_index)
+    print(len(chunk_sample_index))
 
 
 
